4f0068bb58364815148e33fcc5e5b78724e20a5a
lzx897 <liuzx53@mail2.sysu.edu.cn>
2023-03-10 11:35:07 +0800

    Site updated: 2023-03-10 11:35:06

---
+++ js/bundle.js
@@ -0,0 +1,5 @@
+exports.ResourcesPath = '/Murasame';
+exports.BackImageName = ''; // background image
+exports.GearImageName = ''; // image of gear for switching models
+exports.PowerImageName = ''; // not important
+exports.ModelDir = ['Murasame']; // names of models
